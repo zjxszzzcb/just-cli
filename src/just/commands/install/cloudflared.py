e49483b7a3ef0c6765ddce6eacd3d85e9dc0a399
zzzcb <zjxs.zzzcb@gmail.com>
2025-10-24 00:41:38 +0800

    feat: add cloudflare tunnel command

---
+++ src/just/commands/install/cloudflared.py
@@ -0,0 +1,15 @@
+import os
+import platform
+
+from just import capture_exception, echo
+from .entry import install_cli
+
+
+@install_cli.command(name="cloudflare", help="Install Cloudflare toolkit.")
+@capture_exception
+def install_cloudflared():
+    if platform.system() == "Windows":
+        os.system('winget install --id Cloudflare.cloudflared')
+    else:
+        msg = f"The installation of 'cloudflare' on the '{platform.system()}' platform has not been implemented."
+        echo.error(msg)
